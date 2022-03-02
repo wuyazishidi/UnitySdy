6e0be4cb55e252d2e73b9666b252b765ba4a0e27
jwp <2604591896@qq.com>
2022-03-02 15:03:40 +0800

    1.随机生成名字

---
+++ Assets/Scrips/Common/PathDefine.cs
@@ -0,0 +1,16 @@
+/****************************************************
+    文件：PathDefine.cs
+	作者：靳万朋
+    邮箱: 2604591896@qq.com
+    日期：2022/3/2 10:17:3
+	功能：路径常量宣言
+*****************************************************/
+
+using UnityEngine;
+
+public class PathDefine 
+{
+    #region Configs
+    public const string RDNameCfg = "ResCfgs/rdname";
+    #endregion 
+}
